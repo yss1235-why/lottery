bfec30d48965aa9a21dc9c8fdf47472da7bc218b
yss1235-why <cofclanone1@gmail.com>
2025-04-09 20:51:21 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 module.exports = {
   extends: 'next/core-web-vitals',
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     'react-hooks/exhaustive-deps': 'warn',
     'react/no-unescaped-entities': 'off',
     '@typescript-eslint/no-unused-vars': 'warn'
